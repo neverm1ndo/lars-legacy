a3b92238e37a5092afaee1344ea3c7ced0049621
neverm1ndo <reddigitalsw@gmail.com>
2020-11-07 09:26:59 +0300

    update dashboard

--- src/app/home/dashboard/dashboard.component.ts
+++ src/app/home/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, HostListener } from '@angular/core';
+import { faFolder, faSearch, faSearchPlus, faUpload } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-dashboard',
@@ -10,6 +11,13 @@ export class DashboardComponent implements OnInit {
    license: boolean;
    innerHeight: number;
    version: string = '1.0';
+
+   fa = {
+     dir: faFolder,
+     search: faSearch,
+     searchp: faSearchPlus,
+     up: faUpload
+   }
 
    @HostListener('window:resize', ['$event'])
      onResize() {
--- src/app/home/search/search.component.ts
+++ src/app/home/search/search.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Output, EventEmitter, TemplateRef, ViewChild } from '@angular/core';
+import { Component, OnInit, Output, Input, EventEmitter, TemplateRef, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { faFilter, faSync, faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 import { ApiService } from '../../api.service';
 import { ToastService } from '../../toast.service';
@@ -25,13 +26,15 @@ export class SearchComponent implements OnInit {
   }
 
   @Output() searchQuery = new EventEmitter<string>();
+  @Input('quick') quick: boolean = false;
 
   @ViewChild('error')
   private error: TemplateRef<any>;
 
   constructor(
     public api: ApiService,
-    public toast: ToastService
+    public toast: ToastService,
+    public router: Router
   ) { }
 
   get query() { return this.searchForm.get('query'); }
@@ -39,6 +42,9 @@ export class SearchComponent implements OnInit {
   sendQuery(): void {
     if (this.searchForm.valid) {
       this.searchQuery.emit(this.query.value);
+      if (this.quick) {
+        this.router.navigate(['home/search'])
+      }
     } else {
         this.toast.show(this.error, { classname: 'bg-danger text-light', delay: 3000 });
     }
