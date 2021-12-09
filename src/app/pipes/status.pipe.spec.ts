784cfb294783557dfebe69cc73a81832cdebfb63
neverm1ndo <it@nmnd.ru>
2021-12-09 10:58:19 +0300

    (tests) cover status pipe with tests

--- src/app/pipes/status.pipe.spec.ts
+++ src/app/pipes/status.pipe.spec.ts
@@ -1,8 +1,16 @@
 import { StatusPipe } from './status.pipe';
 
 describe('StatusPipe', () => {
-  xit('create an instance', () => {
+  it('create an instance', () => {
     const pipe = new StatusPipe();
     expect(pipe).toBeTruthy();
+  });
+  it('should transform status into the readable form', () => {
+    const pipe = new StatusPipe();
+    expect(pipe.transform('rebooting')).toBe('Перезагружается');
+    expect(pipe.transform('live')).toBe('Запущен');
+    expect(pipe.transform('stoped')).toBe('Остановлен');
+    expect(pipe.transform('loading')).toBe('Загружается');
+    expect(pipe.transform('error')).toBe('Ошибка сервера');
   });
 });
