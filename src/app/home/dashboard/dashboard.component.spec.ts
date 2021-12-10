1ae619dbce0a94758697768c7dfd0009028998e2
neverm1ndo <it@nmnd.ru>
2021-12-10 12:18:53 +0300

    (tests dashboard) cover dashboard component with tests

--- src/app/home/dashboard/dashboard.component.spec.ts
+++ src/app/home/dashboard/dashboard.component.spec.ts
@@ -19,7 +19,7 @@ describe('DashboardComponent', () => {
     fixture.detectChanges();
   });
 
-  xit('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
