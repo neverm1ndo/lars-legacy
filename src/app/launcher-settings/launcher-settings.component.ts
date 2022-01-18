7203a2476703a5ad4a8c1a401e34b68801b2c9af
neverm1ndo <it@nmnd.ru>
2022-01-18 14:48:24 +0300

    (settings launcher) set settings form

--- src/app/launcher-settings/launcher-settings.component.ts
+++ src/app/launcher-settings/launcher-settings.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { join } from 'path';
+import { faFolderOpen } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-launcher-settings',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LauncherSettingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+  ) { }
+
+  fa = {
+    folder: faFolderOpen
+  }
+
+  settings = new FormGroup({
+    samp: new FormControl(join(process.env['ProgramFiles(x86)'], 'GTASanAndreas')),
+    nickname: new FormControl(JSON.parse(localStorage.getItem('user')).name),
+    password: new FormControl('')
+  });
+
+  get sets() {
+    return this.settings.value;
+  }
+
+  setup() {
+    localStorage.setItem('launcher', JSON.stringify(this.sets));
+  }
+
+  getLauncherSettingsFromStorage(): void {
+    try {
+      if (!localStorage.getItem('launcher')) throw new Error('EMPTY_LAUNCHER_SETTINGS');
+      this.settings.setValue(JSON.parse(localStorage.getItem('launcher')))
+    } catch(err) {
+      this.setup();
+      console.warn(err.message, 'Launcher settings reset to default');
+    }
+  }
 
   ngOnInit(): void {
+    this.getLauncherSettingsFromStorage();
   }
 
 }
