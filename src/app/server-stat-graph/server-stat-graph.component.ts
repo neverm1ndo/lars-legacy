1bff56e80a5e44c1a1ecd1d6355549cede2471c2
neverm1ndo <it@nmnd.ru>
2022-04-28 23:41:03 +0300

    (server-stat-graph) code optimizations

--- src/app/server-stat-graph/server-stat-graph.component.ts
+++ src/app/server-stat-graph/server-stat-graph.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit, OnDestroy, ViewChild, ElementRef, NgZone, ChangeDetectorRef, ChangeDetectionStrategy, Input } from '@angular/core';
 import { ElectronService } from '../core/services';
 import { faUsers, faServer, faLock, faLockOpen } from '@fortawesome/free-solid-svg-icons';
+import { IconDefinition } from '@fortawesome/fontawesome-common-types';
 import { AppConfig } from '../../environments/environment';
 import { ServerGameMode } from '../../../samp';
-import { interval } from 'rxjs';
-import { filter } from 'rxjs/operators';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'server-stat-graph',
@@ -21,20 +21,20 @@ export class ServerStatGraphComponent implements OnInit, OnDestroy {
   ) { }
 
   @Input('players') set players(players: number) {
-    if (this.stat) {
-      this.stat.players.online = players;
-      this.points.push(players);
-      this.draw();
-    }
+    if (!this.stat) return;
+    this.stat.players.online = players;
+    this.points.push(players);
+    this.drawGraphics();
   }
 
   points: number[] = [];
   stat: ServerGameMode;
 
-  timer = interval(60000)
-  .subscribe(() => { if (this.stat) { this.points.push(this.stat.players.online); this.draw(); }});
+  timer: Subscription = interval(60000).subscribe(() => {
+    this.players = this.stat.players.online;
+  });
 
-  fa = {
+  fa: { [iconName: string]: IconDefinition } = {
     users: faUsers,
     server: faServer,
     locked: faLock,
@@ -43,52 +43,63 @@ export class ServerStatGraphComponent implements OnInit, OnDestroy {
 
   @ViewChild('graphics') graphics: ElementRef<HTMLCanvasElement>;
 
-  getMaxPoint() {
-     return Math.max.apply(null, this.points);
+  getMaxExistingPoint(): number {
+    return Math.max.apply(null, this.points);
   }
-  draw() {
+
+  drawGraphics(): void {
     this.zone.runOutsideAngular(() => {
-      const ctx = this.graphics.nativeElement.getContext('2d');
-      const height = 64;
-      let offset = 0;
-      let top = 0;
-      let maxPoint = this.getMaxPoint();
-      if (maxPoint <= 64) top = 64;
+      const WHITE: string = '#afafaf';
+      const PRIMARY: string = '#fd7e14';
+      const MAX_PLAYERS: number = 64;
+      const ctx: CanvasRenderingContext2D = this.graphics.nativeElement.getContext('2d');
+      const height: number = MAX_PLAYERS;
+      let offset: number = 0;
+      let top: number = 0;
+      let maxPoint: number = this.getMaxExistingPoint();
+
+      if (maxPoint <= MAX_PLAYERS) top = MAX_PLAYERS;
       if (maxPoint <= 32) top = 32;
       if (maxPoint <= 10) top = 12;
 
+      /**
+      * Draw graphics grid, max definition depends from max players
+      **/
       const drawGrid = () => {
         const grid = new Path2D();
-              ctx.strokeStyle = '#afafaf';
+              ctx.strokeStyle = WHITE;
               ctx.lineWidth = 0.1;
         for (let i = 1; i < 4; i++) {
-            grid.moveTo(15, 64/4*i);
-            grid.lineTo(270, 64/4*i);
+            grid.moveTo(15, MAX_PLAYERS/4*i);
+            grid.lineTo(270, MAX_PLAYERS/4*i);
             grid.closePath();
-            ctx.fillStyle = '#afafaf';
-            ctx.fillText(String(top/4*(4 - i)), 0, 64/4*i + 4);
+            ctx.fillStyle = WHITE;
+            ctx.fillText(String(top/4*(4 - i)), 0, MAX_PLAYERS/4*i + 4);
         }
         for (let i = 1; i < 25; i++) {
             grid.moveTo(270/10*i, 0);
             grid.lineTo(270/10*i, height);
             grid.closePath();
         }
-         ctx.stroke(grid);
-      }
+        ctx.stroke(grid);
+      };
+
       if (offset >= 250) this.points.shift();
       ctx.clearRect(0, 0, 275, height);
       ctx.beginPath();
-      const area = new Path2D();
+
+      const area: Path2D = new Path2D();
+
       area.moveTo(20, height - this.points[0]*(height/top));
       for (let i = 0; i < this.points.length; i++) {
-        offset=10*i;
+        offset = 10*i;
         area.lineTo(20 + offset, height - this.points[i]*(height/top));
       }
       area.lineTo(20 + offset, height);
       area.lineTo(20, height)
       area.closePath();
-      ctx.strokeStyle = '#fd7e14';
-      ctx.fillStyle = '#fd7e14';
+      ctx.strokeStyle = PRIMARY;
+      ctx.fillStyle = PRIMARY;
       ctx.stroke(area);
       ctx.fill(area);
       drawGrid();
@@ -96,23 +107,24 @@ export class ServerStatGraphComponent implements OnInit, OnDestroy {
     this.cfr.detectChanges();
   }
 
-  async getServerInfo() {
-    return this.electron.ipcRenderer.invoke('server-game-mode', new URL(AppConfig.api.main).host, 7777)
-    .then((info: ServerGameMode) => {
-      this.stat = info;
-      this.points.push(info.players.online);
-      return Promise.resolve();
-    });
+  async getServerInfo(): Promise<void> {
+    const PORT: number = 7777;
+    return this.electron.ipcRenderer.invoke('server-game-mode', new URL(AppConfig.api.main).host, PORT)
+               .then((info: ServerGameMode) => {
+                 this.stat = info;
+                 this.points.push(info.players.online);
+                 return Promise.resolve();
+               });
   }
 
   ngOnInit(): void {
     this.getServerInfo()
         .then(() => {
-          this.draw();
+          this.drawGraphics();
         });
   }
+
   ngOnDestroy(): void {
     this.timer.unsubscribe();
   }
-
 }
