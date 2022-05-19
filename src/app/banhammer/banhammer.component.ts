cb7cd0348e132a4eea5fb38a3393874527f3ea33
neverm1ndo <it@nmnd.ru>
2022-05-19 21:28:40 +0300

    (banlist) add ban types

--- src/app/banhammer/banhammer.component.ts
+++ src/app/banhammer/banhammer.component.ts
@@ -1,6 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { faUserSlash } from '@fortawesome/free-solid-svg-icons';
 
+enum BanType {
+  CHEATING,
+  BUG_ABUSE,
+  ADS,
+  PAUSING,
+  DRIVEBY,
+  REJECTED_NICKNAME,
+  INSULT,
+}
+
+interface BanRule {
+  id: number;
+  rule: string;
+  ban_type: BanType;
+  ip: string;
+  serial_cn?: string;
+  serial_as?: string;
+  serial_ss?: string;
+  user_id?: number;
+  admin_id: number;
+  banned_from: Date;
+  banned_to: Date | null;
+}
+
 @Component({
   selector: 'app-banhammer',
   templateUrl: './banhammer.component.html',
@@ -10,14 +34,17 @@ export class BanhammerComponent implements OnInit {
 
   constructor() { }
 
-  searchTypes = [
+  public bans: BanRule[] = [];
+
+  public searchTypes = [
     { id: 0, val: 'IP' },
     { id: 1, val: 'CN' },
     { id: 2, val: 'AS&SS' },
     { id: 3, val: 'Никнейм' },
     { id: 4, val: 'Время' },
   ];
-  currentSearchType: number = 0;
+
+  public currentSearchType: number = 0;
 
   fa = {
     ban: faUserSlash
