793b7a4a439c54ca889878807241a504289de541
Mikhail <it@nmnd.ru>
2022-05-19 21:09:57 +0300

    banlist component (#15)
    * (bans) add template
    
    * (bans) add empty template

--- src/app/banhammer/banhammer.component.ts
+++ src/app/banhammer/banhammer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { faGavel } from '@fortawesome/free-solid-svg-icons';
+import { faUserSlash } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-banhammer',
@@ -10,9 +10,18 @@ export class BanhammerComponent implements OnInit {
 
   constructor() { }
 
+  searchTypes = [
+    { id: 0, val: 'IP' },
+    { id: 1, val: 'CN' },
+    { id: 2, val: 'AS&SS' },
+    { id: 3, val: 'Никнейм' },
+    { id: 4, val: 'Время' },
+  ];
+  currentSearchType: number = 0;
+
   fa = {
-    gavel: faGavel
-  };
+    ban: faUserSlash
+  }
 
   ngOnInit(): void {
   }
