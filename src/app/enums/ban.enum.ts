9dd22b92bcc134aad994edb2d714f8d0b57a59eb
neverm1ndo <it@nmnd.ru>
2022-09-08 14:10:23 +0300

    (banhammer) add Ban enum file

---
+++ src/app/enums/ban.enum.ts
@@ -0,0 +1,17 @@
+export enum BanType {
+    CHEATING,
+    BUG_ABUSE,
+    ADS,
+    PAUSING,
+    DRIVEBY,
+    REJECTED_NICKNAME,
+    INSULT,
+  }
+  
+  export enum SearchType {
+    IP,
+    CN,
+    SERIALS,
+    DATE,
+    ANY,
+  }
