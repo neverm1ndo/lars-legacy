ae1422857facfc28800e8c76b64a1c58f45b3084
neverm1ndo <it@nmnd.ru>
2022-12-19 13:40:58 +0300

    feature (geo) compact geo

--- src/app/home/geo/geo.component.ts
+++ src/app/home/geo/geo.component.ts
@@ -1,7 +1,8 @@
 import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
 import { IGeoData } from '@lars/interfaces/app.interfaces';
-import { faLink } from '@fortawesome/free-solid-svg-icons';
+import { faClipboardList, faLink } from '@fortawesome/free-solid-svg-icons';
 import { Router } from '@angular/router';
+import { ToastService } from '@lars/toast.service';
 
 @Component({
   selector: 'geo',
@@ -13,15 +14,33 @@ export class GeoComponent {
 
   @Input('table') table: IGeoData;
 
-  fa = { link : faLink };
+  fa = { 
+    link : faLink,
+    clipboard: faClipboardList
+  };
 
   constructor(
-    public router: Router
+    public router: Router,
+    private _toast: ToastService,
   ) { }
 
   public sendQuery(): void {
     const geoQuery = `srl:${this.table.as.toString()}*${this.table.ss}`;
     this.router.navigate(['home/search'], { queryParams: { query: geoQuery }})
+  }
+
+  public copyToClipboard(): void {
+    const text: string = Object.entries(this.table)
+                               .reduce((acc: string, [key, value]: [string, any]) => {
+                                  acc += `${key}:${value}\r\n`;
+                                  return acc;
+                               }, '');
+    navigator.clipboard.writeText(text)
+                       .then(() => {
+                          this._toast.show('success', 'Геодата скопирована в буффер обмена', null, faClipboardList);
+                       }, () => {
+                          this._toast.show('warning', 'Ошибка доступа к буфферу обмена');
+                       });
   }
   
   public sendQueryIP(): void {
