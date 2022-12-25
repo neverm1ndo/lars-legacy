33b15e0aa9421e5909382f8f3c078275200733ff
neverm1ndo <it@nmnd.ru>
2022-12-25 11:59:36 +0300

    fix (backups) repair broken ui and rendering queue

--- src/app/home/backups/backups.component.ts
+++ src/app/home/backups/backups.component.ts
@@ -3,11 +3,12 @@ import { ApiService } from '@lars/api.service';
 import { ToastService } from '@lars/toast.service';
 import { ElectronService } from '@lars/core/services';
 import { faClipboardCheck, faClipboard, faFileSignature, faExclamationCircle, faTrash, faBoxOpen, faHdd } from '@fortawesome/free-solid-svg-icons';
-import { catchError, take, map, switchMap, filter, mergeMap } from 'rxjs/operators'
-import { from, iif, of, throwError, Observable, tap } from 'rxjs';
+import { catchError, take, map, switchMap, filter, mergeMap, takeUntil } from 'rxjs/operators'
+import { from, iif, of, throwError, Observable, tap, BehaviorSubject, Subject } from 'rxjs';
 import { handleError } from '@lars/utils';
 import { Backup } from '@lars/interfaces';
 import { IOutputAreaSizes } from 'angular-split';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-backups',
@@ -21,8 +22,8 @@ export class BackupsComponent implements OnInit, OnDestroy {
   @ViewChild('backupsList') backupsItems: ElementRef<HTMLDivElement>;
   
   public $backups: Observable<Backup[]> = this._api.getBackupsList();
-
-  public current: Backup;
+  public $current: BehaviorSubject<Backup> = new BehaviorSubject(null);
+  public $error: Subject<Error> = new Subject();
 
   public paneStates: number[] = [];
 
@@ -194,23 +195,16 @@ export class BackupsComponent implements OnInit, OnDestroy {
     drawRibs();
   }
 
-  public getBackupFile(backup: Backup): void {
-    if (backup.file.binary) return;
-    this._api.getBackupFile(backup.hash)
-              .pipe(
-                catchError(error => handleError(error)),
-                take(1)
-              )
-              .subscribe({
-                next: (data) => {
-                    this.current = backup;
-                    this.current.file.text = data;
-                }, 
-                error: (err) => {
-                  console.error(err);
-                  this._toast.show('danger', `Бэкап файла ${backup.file.name} не был загружен для просмотра по причине:`, err.message, faExclamationCircle, 7000);
-                }
-              });
+  private __handleError(error: HttpErrorResponse) {
+    this._toast.show('danger', 'Не удалось загрузить файл бэкапа по причине:', JSON.parse(error.error).message, faExclamationCircle)
+    return throwError(() => error);                               
+  }
+
+  public getBackupFile(backup: Backup): Observable<string> {  
+    return this._api.getBackupFile(backup.hash)
+                    .pipe(
+                      catchError(error => this.__handleError(error),
+                    ));
   }
 
   public removeBackup(): void {
@@ -219,26 +213,46 @@ export class BackupsComponent implements OnInit, OnDestroy {
     */
   }
 
-  public restoreBackup(): void {
+  public closeView() {
+    this.$current.next(null);
+  }
+
+  public showBackupView(backup: Backup) {
+    this.$current.next(backup);
+    this._api.getBackupFile(backup.hash)
+                    .pipe(
+                      catchError(error => this.__handleError(error)),
+                      take(1)
+                    )
+                    .subscribe((text: string) => {
+                      backup.file.text = text;
+                      this.$current.next(backup);
+                    })
+  }
+
+  public restoreBackup(hash: string): void {
+    const current: Backup = this.$current.value;
+    
     const dialogOpts: Electron.MessageBoxOptions = {
       type: 'warning',
       buttons: ['Да, установить', 'Отмена'],
       title: `Подтверждение установки бэкапа`,
-      message: `Вы точно хотите установиить файл бэкапа ${this.current.file.name}? После подтверждения файл бэкапа ЗАМЕНИТ собой текущий файл ${this.current.file.path}.`
+      message: `Вы точно хотите установиить файл бэкапа ${current.file.name}? После подтверждения файл бэкапа ЗАМЕНИТ собой текущий файл ${this.$current.value.file.path}.`
     };
+    
     from(this._electron.ipcRenderer.invoke('message-box', dialogOpts))
       .pipe(
-        switchMap((val) => iif(() => val.response == 0, this._api.restoreBackup(this.current.hash), throwError(() => new Error('Cancelled by user')))),
+        switchMap((val) => iif(() => val.response == 0, this._api.restoreBackup(hash), throwError(() => new Error('Cancelled by user')))),
         catchError(error => handleError(error)),
         take(1)
       )
       .subscribe({
         next: () => {
-          this._toast.show('success', `Бэкап файла ${this.current.file.name} успешно установлен`, this.current.file.path, faClipboardCheck);
+          this._toast.show('success', `Бэкап файла ${current.file.name} успешно установлен`, current.file.path, faClipboardCheck);
         },
         error: (err) => {
           console.error(err);
-          this._toast.show('danger', `Бэкап файла ${this.current.file.name} не был установлен по причине:`, err.message, faClipboard);
+          this._toast.show('danger', `Бэкап файла ${this.$current.value.file.name} не был установлен по причине:`, err.message, faClipboard);
         }
       });
   }
