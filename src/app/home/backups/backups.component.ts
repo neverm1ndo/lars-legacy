b2bdb40c77638d10bebf8361294d97cb054ee50b
neverm1ndo <it@nmnd.ru>
2022-12-25 19:51:02 +0300

    fix (backups) graph as directive

--- src/app/home/backups/backups.component.ts
+++ src/app/home/backups/backups.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild, ElementRef, Renderer2, OnDestroy, ChangeD
 import { ApiService } from '@lars/api.service';
 import { ToastService } from '@lars/toast.service';
 import { ElectronService } from '@lars/core/services';
-import { faClipboardCheck, faClipboard, faFileSignature, faExclamationCircle, faTrash, faBoxOpen, faHdd } from '@fortawesome/free-solid-svg-icons';
+import { faClipboardCheck, faClipboard, faFileSignature, faExclamationCircle, faTrash, faBoxOpen, faHdd, faToolbox } from '@fortawesome/free-solid-svg-icons';
 import { catchError, take, map, switchMap, filter, mergeMap, takeUntil } from 'rxjs/operators'
 import { from, iif, of, concat, throwError, Observable, tap, BehaviorSubject, Subject, Subscription } from 'rxjs';
 import { handleError } from '@lars/utils';
@@ -34,7 +34,8 @@ export class BackupsComponent implements OnInit, OnDestroy {
     trash: faTrash,
     exCircle: faExclamationCircle,
     box: faBoxOpen,
-    hdd: faHdd
+    hdd: faHdd,
+    toolbox: faToolbox,
   };
 
   public codeMirrorSettings = {
@@ -73,129 +74,6 @@ export class BackupsComponent implements OnInit, OnDestroy {
     return (Math.round((new Date(date).getTime() - new Date().getTime()) / 1000)) < 86400*2;
   }
 
-  private _applyStylesPx(this: any, styles: { [key: string] : any }) {
-    for (let style in styles) {
-      this.style[style] = styles[style] + 'px';
-    };
-  }
-
-  private _createBindBackbone(height: number, color: string, top: number, right: number, filename: string): HTMLDivElement {
-    const line = this._renderer.createElement('div');
-    this._renderer.addClass(line, 'bind');
-    
-    const styles = {
-      height,
-      width: 3,
-      marginLeft: right,
-      marginRight: 8,
-      top,
-    };
-
-    line.style.background = color;
-    line.dataset.filename = filename;
-    line.style.position = 'relative';
-    
-    this._applyStylesPx.call(line, styles);
-
-    return line;
-  }
-
-  private _createBindRib(width: number, color: string, top: number, left: number): HTMLDivElement {
-    const line = this._renderer.createElement('div');
-    this._renderer.addClass(line, 'bind-rib')
-    
-    const styles = {
-      width: (width + 3) + 'px',
-      height: 3,
-      top,
-      left,
-    };
-    
-    line.style.background = color;
-    line.style.position = 'relative';
-
-    this._applyStylesPx.call(line, styles);
-
-    return line;
-  }
-
-  private _drawBackbone(height: number, color: string, top: number, index: number, filename: string): void {
-     this.binds.nativeElement.append(this._createBindBackbone(height, color, top, (index) * 8, filename));
-  }
-  private _drawRib(width: number, color: string, top: number, left: number): void {
-     this.binds.nativeElement.append(this._createBindRib(width, color, top, left * 8));
-  }
-  private _colorGenerator(filenames: any[]): string[] {
-    
-    function hashCode(str: string) {
-      let hash = 0;
-      for (let i = 0; i < str.length; i++) {
-         hash = str.charCodeAt(i) + ((hash << 5) - hash);
-      }
-      return hash;
-    };
-    
-    function intToRGB(i: number): string {
-      let c = (i & 0x00FFFFFF)
-          .toString(16)
-          .toUpperCase();
-      return "00000".substring(0, 6 - c.length) + c;
-    };
-    
-    return filenames.map((filename: string) => '#' + intToRGB(hashCode(filename)));
-  }
-
-  private async _drawGraph(): Promise<void> {
-    const childs: HTMLCollection = this.backupsItems.nativeElement.children;
-    let topMarge: number = this.binds.nativeElement.getBoundingClientRect().top - 23;
-    let bb: any;
-    let prevHeight: number = 0;
-    const drawBackbones = async () => {
-      const uniqueFileNames: Set<string> = new Set();
-      for (let i = 0; i < childs.length; i++) {
-        const filename = childs[i].getAttribute('data-filename');
-        this._renderer.setStyle(childs[i].querySelector('.marker'), 'background', this._colorGenerator([filename])[0]);
-        uniqueFileNames.add(filename);
-      }
-      bb = Array.from(uniqueFileNames)
-                .reduce((acc, curr) => {
-                  return {...acc, [curr]: { color: this._colorGenerator([curr])[0] }};
-                }, {});
-      for (let j = childs.length - 1; j >= 0; j--) {
-        const filename = childs[j].getAttribute('data-filename');
-        if (bb[filename] && !bb[filename].maxTop) bb[filename].maxTop = childs[j].getBoundingClientRect().top - topMarge;
-        if (bb[filename]) bb[filename].minTop = childs[j].getBoundingClientRect().top - topMarge;
-      }
-      let index: number = 0;
-      for (let key in bb) {
-        if (bb[key].maxTop === bb[key].minTop) {
-          delete bb[key];
-          continue;
-        }
-        this._drawBackbone(
-          bb[key].maxTop - bb[key].minTop,
-          bb[key].color,
-          bb[key].minTop - prevHeight,
-          index,
-          key
-        );
-        prevHeight += bb[key].maxTop - bb[key].minTop;
-        index++;
-      }
-    }
-    let ribs: number = 0;
-    const drawRibs = async () => {
-      const bbs = Object.keys(bb);
-      for (let i = 0; i < childs.length; i++) {
-        const filename = childs[i].getAttribute('data-filename');
-        if (!bbs.includes(filename)) continue;
-        ribs++;
-        this._drawRib((bbs.length - 1 - bbs.indexOf(filename)+1)*8, bb[filename].color, childs[i].getBoundingClientRect().top - topMarge - prevHeight - ribs*3, bbs.indexOf(filename));
-      }
-    };
-    drawBackbones();
-    drawRibs();
-  }
 
   private __handleError(error: HttpErrorResponse) {
     this._toast.show('danger', 'Не удалось загрузить файл бэкапа по причине:', JSON.parse(error.error).message, faExclamationCircle)
@@ -205,14 +83,9 @@ export class BackupsComponent implements OnInit, OnDestroy {
   public getBackupFile(backup: Backup): Observable<string> {  
     return this._api.getBackupFile(backup.hash)
                     .pipe(
-                      catchError(error => this.__handleError(error),
-                    ));
-  }
-
-  public removeBackup(): void {
-    /**
-    * Not Implemented;
-    */
+                      catchError(error => this.__handleError(error)),
+                      take(1)
+                    );
   }
 
   public closeView() {
@@ -232,7 +105,34 @@ export class BackupsComponent implements OnInit, OnDestroy {
                     })
   }
 
-  public restoreBackup(hash: string): void {
+  public removeBackup(): void {
+    const current: Backup = this.$current.value;
+
+    const dialogOpts: Electron.MessageBoxOptions = {
+      type: 'warning',
+      buttons: ['Удалить', 'Отмена'],
+      title: `Подтверждение удаления бэкапа`,
+      message: `Вы точно хотите удалить файл бэкапа ${current.file.name}? После подтверждения файл бэкапа и запись о нем безвозвратно удалятся.`
+    };
+
+    from(this._electron.ipcRenderer.invoke('message-box', dialogOpts))
+    .pipe(
+      switchMap((val) => iif(() => val.response == 0, this._api.removeBackup(current.hash), throwError(() => new Error('Операция отменена пользователем')))),
+      catchError(error => handleError(error)),
+      take(1)
+    )
+    .subscribe({
+      next: () => {
+        this._toast.show('success', `Бэкап файла ${current.file.name} успешно установлен`, current.file.path, faClipboardCheck);
+      },
+      error: (err) => {
+        console.error(err);
+        this._toast.show('danger', `Бэкап файла ${this.$current.value.file.name} не был удален по причине:`, err.message, faClipboard);
+      }
+    });
+  }
+
+  public restoreBackup(): void {
     const current: Backup = this.$current.value;
     
     const dialogOpts: Electron.MessageBoxOptions = {
@@ -244,7 +144,7 @@ export class BackupsComponent implements OnInit, OnDestroy {
     
     from(this._electron.ipcRenderer.invoke('message-box', dialogOpts))
       .pipe(
-        switchMap((val) => iif(() => val.response == 0, this._api.restoreBackup(hash), throwError(() => new Error('Cancelled by user')))),
+        switchMap((val) => iif(() => val.response == 0, this._api.restoreBackup(current.hash), throwError(() => new Error('Операция отменена пользователем')))),
         catchError(error => handleError(error)),
         take(1)
       )
@@ -254,7 +154,7 @@ export class BackupsComponent implements OnInit, OnDestroy {
         },
         error: (err) => {
           console.error(err);
-          this._toast.show('danger', `Бэкап файла ${this.$current.value.file.name} не был установлен по причине:`, err.message, faClipboard);
+          this._toast.show('danger', `Бэкап файла ${current.file.name} не был установлен по причине:`, err.message, faClipboard);
         }
       });
   }
@@ -268,14 +168,9 @@ export class BackupsComponent implements OnInit, OnDestroy {
       .subscribe(({ textEditorStyle }) => {
         this.codeMirrorSettings.theme = textEditorStyle;
       });
-    
-      this._graphDrawSubscription = this.$backups.subscribe({
-        next: () => setTimeout(() => this._drawGraph(), 0),
-      });
+
   }
 
-  ngOnDestroy() {
-    this._graphDrawSubscription.unsubscribe();
-  }
+  ngOnDestroy() {}
 
 }
