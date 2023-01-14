cd91eabbb89063866432610ccbd07c5533f212cf
neverm1ndo <it@nmnd.ru>
2023-01-14 08:14:44 +0300

    chore environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,11 +5,11 @@ export const AppConfig = {
     resource: 'https://www.gta-liberty.ru/',
     server_monitor: 'https://apps.nmnd.ru/api/samp'
   },
-  api: { // Dev server
+  api: {
     socket: 'wss://svr.gta-liberty.ru/',
-    main: 'https://svr.gta-liberty.ru/v2/',
-    auth: 'https://svr.gta-liberty.ru/v2/login',
-    user: 'https://svr.gta-liberty.ru/v2/login/user',
+    main: 'https://svr.gta-liberty.ru/v2/lars/',
+    auth: 'https://svr.gta-liberty.ru/v2/auth/',
+    user: 'https://svr.gta-liberty.ru/v2/auth/user',
     validation: 'https://svr.gta-liberty.ru/v2/login/check-token'
   }
 };
