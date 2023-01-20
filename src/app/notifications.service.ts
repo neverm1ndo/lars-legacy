3363c909fd2384871133f99d88b9fcfe2af6e083
neverm1ndo <it@nmnd.ru>
2023-01-20 11:27:03 +0300

    chore notifications

--- src/app/notifications.service.ts
+++ src/app/notifications.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { WebSocketService } from '@lars/web-socket.service';
 import { Subscription, TeardownLogic} from 'rxjs';
-import { filter, switchMap } from 'rxjs/operators';
-import { NgxIndexedDBService } from 'ngx-indexed-db';
+import { filter } from 'rxjs/operators';
 import { getProcessTranslation } from '@lars/shared/components/line-process/log-processes';
 
 @Injectable({
@@ -13,7 +12,7 @@ export class NotificationsService {
   private _notifications: Subscription = new Subscription();
   
   constructor(
-    private ws: WebSocketService,
+    private _ws: WebSocketService,
   ) {}
 
   spawnNotification(title: string, body: string, image?: string) {
@@ -34,23 +33,23 @@ export class NotificationsService {
 
   subToNotifications() {
     const subscriptions: TeardownLogic[] = [
-      this.ws.getAlertGuardBlockOn()
+      this._ws.getAlertGuardBlockOn()
              .pipe(filter(() => !!JSON.parse(window.localStorage.getItem('alerts')).autoBlock))
              .subscribe((line) => {
                 this.spawnNotification(`Кикбан ${line.nickname}`, `${line.nickname} заблокирован системой по причине ${line.content.message}`);
              }),
-      this.ws.getAlertKickban()
+      this._ws.getAlertKickban()
              .pipe(filter(() => !!JSON.parse(window.localStorage.getItem('alerts')).autoBan))
              .subscribe((line) => {
                this.spawnNotification(`Кикбан ${line.nickname}`, `${line.nickname} кикнут системой по причине: ${getProcessTranslation(line.process)}`);
              }),
-      this.ws.getServerStopNotification()
+      this._ws.getServerStopNotification()
              .pipe(
                 filter(() => !!JSON.parse(window.localStorage.getItem('alerts')).serverShutdown),
              ).subscribe((user: any) => {
                 this.spawnNotification('Сервер остановлен', `${user.name} остановил работу сервера`, user.user_avatar);
              }),
-      this.ws.getServerRebootNotification()
+      this._ws.getServerRebootNotification()
       .pipe(
           filter(() => !!JSON.parse(window.localStorage.getItem('alerts')).serverShutdown),
       ).subscribe((user: any) => {
