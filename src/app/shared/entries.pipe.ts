8a0f7a507fba59b74acd99e6519b68749138589b
neverm1ndo <it@nmnd.ru>
2023-02-23 20:35:02 +0300

    new (loglines) add new logline schema

---
+++ src/app/shared/entries.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'entries'
+})
+export class EntriesPipe implements PipeTransform {
+
+  transform(value: unknown): any[] {
+    return Object.entries(value);
+  }
+}
