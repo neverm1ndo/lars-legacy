d1f2b6291a96f6943c4d0a02d9d1bdb4a9bb95f2
neverm1ndo <it@nmnd.ru>
2024-01-09 22:46:32 +0300

    i18n(backups) add localization tokens

--- src/app/backups/containers/backups.component.ts
+++ src/app/backups/containers/backups.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit, OnDestroy, ChangeDetectionStrategy, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectionStrategy, ViewChild, AfterViewInit, Optional } from '@angular/core';
 import { ApiService } from '@lars/api/api.service';
 import { ToastService } from '@lars/toast.service';
 import { ElectronService } from '@lars/core/services';
 import { faClipboardCheck, faClipboard, faFileSignature, faExclamationCircle, faTrash, faBoxOpen, faHdd, faToolbox } from '@fortawesome/free-solid-svg-icons';
-import { catchError, take, map, switchMap, filter } from 'rxjs/operators'
+import { catchError, take, map, switchMap, filter, tap } from 'rxjs/operators'
 import { from, iif, of, throwError, Observable, BehaviorSubject, Subject, merge, debounceTime } from 'rxjs';
 import { handleError } from '@lars/utils';
 import { Backup } from '@lars/interfaces';
 import { IOutputAreaSizes } from 'angular-split';
 import { HttpErrorResponse } from '@angular/common/http';
 import { BackupsGraphDirective } from '../components/backups-graph/backups-graph.directive';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-backups',
@@ -18,185 +19,239 @@ import { BackupsGraphDirective } from '../components/backups-graph/backups-graph
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BackupsComponent implements OnInit, OnDestroy, AfterViewInit {
+	@ViewChild('graph') private _graph: BackupsGraphDirective;
+
+	public $backups: BehaviorSubject<Backup[]> = new BehaviorSubject(null);
+	private $currentSubject: BehaviorSubject<Backup> = new BehaviorSubject(null);
+	public $current: Observable<Backup> = merge(this.$currentSubject, this.$currentSubject.pipe(
+											filter((backup) => backup !== null),
+											debounceTime(500),
+											switchMap((backup: Backup) => this._getBackupFile(backup.hash)
+																				.pipe(map((text: string) => {
+																				backup.file.text = text;
+																				return backup;
+																				})))
+											));
+
+	public $error: Subject<Error> = new Subject();
+
+	public paneStates: number[] = [];
+
+	public fa = {
+	sign: faFileSignature,
+	trash: faTrash,
+	exCircle: faExclamationCircle,
+	box: faBoxOpen,
+	hdd: faHdd,
+	toolbox: faToolbox,
+	};
+
+	public codeMirrorSettings = {
+	lineNumbers: true,
+	theme: 'dracula',
+	lineWrapping: true,
+	readOnly: true
+	};
+
+	public $backupsSize: Subject<number> = new Subject();
+
+	constructor(
+	private _api: ApiService,
+	private _toast: ToastService,
+	@Optional() private _electron: ElectronService,
+	private translateService: TranslateService
+	) {
+	this.paneStates = this._setPanesState();
+	this.translateService.use('ru');
+	}
 
 
-  @ViewChild('graph') private _graph: BackupsGraphDirective;
-  
-  public $backups: BehaviorSubject<Backup[]> = new BehaviorSubject(null);
-  private $currentSubject: BehaviorSubject<Backup> = new BehaviorSubject(null);
-  public $current: Observable<Backup> = merge(this.$currentSubject, this.$currentSubject.pipe(
-                                            filter((backup) => backup !== null),
-                                            debounceTime(500),
-                                            switchMap((backup: Backup) => this._getBackupFile(backup.hash)
-                                                                              .pipe(map((text: string) => {
-                                                                                backup.file.text = text;
-                                                                                return backup;
-                                                                              })))
-                                         ));
-  
-  public $error: Subject<Error> = new Subject();
+	public savePanesState(event: { gutterNum: number | '*', sizes: IOutputAreaSizes }): void {
+	window.localStorage.setItem('lars/ui/panes/backups', JSON.stringify(event.sizes));
+	}
 
-  public paneStates: number[] = [];
+	private _setPanesState(): number[] {
+	try {
+		const states = JSON.parse(window.localStorage.getItem('lars/ui/panes/backups'));
+		if (!states) throw 'undefined states';
+		return states;
+	} catch(err) {
+		console.error(err);
+		return [20, 80];
+	}
+	}
 
-  public fa = {
-    sign: faFileSignature,
-    trash: faTrash,
-    exCircle: faExclamationCircle,
-    box: faBoxOpen,
-    hdd: faHdd,
-    toolbox: faToolbox,
-  };
-
-  public codeMirrorSettings = {
-    lineNumbers: true,
-    theme: 'dracula',
-    lineWrapping: true,
-    readOnly: true
-  };
-
-  public $backupsSize: Subject<number> = new Subject();
-  
-  constructor(
-    private _api: ApiService,
-    private _toast: ToastService,
-    private _electron: ElectronService,
-  ) {
-    this.paneStates = this._setPanesState();
-  }
+	public willBeDeletedSoon(date: Date): boolean {
+	return (Math.round((new Date(date).getTime() - new Date().getTime()) / 1000)) < 86400*2;
+	}
 
 
-  public savePanesState(event: { gutterNum: number | '*', sizes: IOutputAreaSizes }): void {
-    window.localStorage.setItem('lars/ui/panes/backups', JSON.stringify(event.sizes));
-  }
+	private __handleError(error: HttpErrorResponse) {
+	this._toast.show('danger',
+		this.translateService.instant('Backups.Toast.LoadError'),
+		JSON.parse(error.error).message,
+		faExclamationCircle
+	);
+	return throwError(() => error);                               
+	}
 
-  private _setPanesState(): number[] {
-    try {
-      const states = JSON.parse(window.localStorage.getItem('lars/ui/panes/backups'));
-      if (!states) throw 'undefined states';
-      return states;
-    } catch(err) {
-      console.error(err);
-      return [20, 80];
-    }
-  }
+	public closeView() {
+	this.$currentSubject.next(null);
+	}
 
-  public willBeDeletedSoon(date: Date): boolean {
-    return (Math.round((new Date(date).getTime() - new Date().getTime()) / 1000)) < 86400*2;
-  }
+	public showBackupView(backup: Backup) {
+	this.$currentSubject.next(backup);
+	}
 
+	public removeBackup(): void {
+		const current: Backup = this.$currentSubject.value;
 
-  private __handleError(error: HttpErrorResponse) {
-    this._toast.show('danger', 'Не удалось загрузить файл бэкапа по причине:', JSON.parse(error.error).message, faExclamationCircle)
-    return throwError(() => error);                               
-  }
+		this.translateService.get([
+			"Backups.Dialog.Delete.Confirm",
+			"Backups.Dialog.Delete.Cancel",
+			"Backups.Dialog.Delete.Title",
+			"Backups.Dialog.Delete.Message",
+		], { filename: current.file.name, path: current.file.path }).pipe(
+			map((translations) => Object.values(translations)),
+			map(([confirm, cancel, title, message]: string[]) => ({
+				type: 'warning',
+				buttons: [
+					cancel,
+					confirm, 
+				],
+				title,
+				message
+			})),
+			switchMap((options: Electron.MessageBoxOptions) => from(this._electron.ipcRenderer.invoke('message-box', options))),
+			switchMap(
+				(val) => iif(
+					() => val.response == 1,
+					this._api.removeBackup(current.hash),
+					this.translateService.get('Backups.Dialog.ClientResponseRejectError').pipe(
+						switchMap((error) => throwError(() => new Error(error)))
+					)
+				)
+			),
+			switchMap(() => this._api.getBackupsList()),
+			catchError(error => handleError(error)),
+			take(1)
+		).subscribe(
+			{
+				next: (backups: Backup[]) => {
+					this.$backups.next(backups);
+					this._toast.show('success',
+						this.translateService.instant('Backups.Toast.DeleteSuccess', { filename: current.file.name }),
+						current.file.path, 
+						faClipboardCheck
+					);
+					this.closeView();
+				},
+				error: (err) => {
+					console.error(err);
+						this._toast.show('danger',
+						this.translateService.instant('Backups.Toast.DeleteError', { filename: current.file.name }),
+						err.message,
+						faClipboard
+					);
+				}
+			}
+		);
+	}
 
-  public closeView() {
-    this.$currentSubject.next(null);
-  }
+	public restoreBackup(): void {
+	const current: Backup = this.$currentSubject.value;
 
-  public showBackupView(backup: Backup) {
-    this.$currentSubject.next(backup);
-  }
+	this.$currentSubject.pipe(
+		map(({ file }) => file),
+		switchMap((file) => this.translateService.get([
+			"Backups.Dialog.Install.Confirm",
+			"Backups.Dialog.Install.Cancel",
+			"Backups.Dialog.Install.Title",
+			"Backups.Dialog.Install.Message",
+		], { filename: file.name, path: file.path })),
+		map((translations) => Object.values(translations)),
+		map(([confirm, cancel, title, message]: string[]) => ({
+			type: 'warning',
+			buttons: [cancel, confirm],
+			title,
+			message
+		})),
+		switchMap((options: Electron.MessageBoxOptions) => from(this._electron.ipcRenderer.invoke('message-box', options))),
+		switchMap(
+			(val: Electron.MessageBoxReturnValue) => iif(
+				() => val.response === 1,
+				this._api.restoreBackup(current.hash),
+				this.translateService.get('Backups.Dialog.ClientResponseRejectError').pipe(
+					switchMap((error) => throwError(() => new Error(error)))
+				)
+			)
+		),
+		catchError(error => handleError(error)),
+		take(1)
+	).subscribe(
+		{
+			next: () => {
+				this._toast.show(
+					'success',
+					this.translateService.instant('Backups.Toast.InstallSuccess', { filename: current.file.name }),
+					current.file.path,
+					faClipboardCheck
+				);
+			},
+			error: (err) => {
+				console.error(err);
+				this._toast.show(
+					'danger',
+					this.translateService.instant('Backups.Toast.InstallError', { filename: current.file.name }),
+					err.message,
+					faClipboard
+				);
+			}
+		});
+	}
 
-  public removeBackup(): void {
-    const current: Backup = this.$currentSubject.value;
+	private _getBackupFile(hash: string): Observable<string> {
+	return this._api.getBackupFile(hash)
+					.pipe(
+						catchError(error => this.__handleError(error)),
+						take(1)
+					);
+	}
 
-    const dialogOpts: Electron.MessageBoxOptions = {
-      type: 'warning',
-      buttons: ['Удалить', 'Отмена'],
-      title: `Подтверждение удаления бэкапа`,
-      message: `Вы точно хотите удалить файл бэкапа ${current.file.name}? После подтверждения файл бэкапа и запись о нем безвозвратно удалятся.`
-    };
+	ngOnInit(): void {
+		of(window.localStorage.getItem('settings'))
+			.pipe(
+				filter((settings: string | null) => settings !== null),
+				map((settings: string) => JSON.parse(settings)),
+				take(1),
+			)
+			.subscribe(({ textEditorStyle }) => {
+				this.codeMirrorSettings.theme = textEditorStyle;
+			});
+		
+		this._api.getBackupsList()
+				.pipe(
+					take(1),
+				)
+				.subscribe((backups: Backup[]) => {
+					this.$backups.next(backups);
+					if (backups.length) this.showBackupView(backups[0]);
+				});
+	}
 
-    from(this._electron.ipcRenderer.invoke('message-box', dialogOpts))
-    .pipe(
-      switchMap((val) => iif(() => val.response == 0, this._api.removeBackup(current.hash), throwError(() => new Error('Операция отменена пользователем')))),
-      switchMap(() => this._api.getBackupsList()),
-      catchError(error => handleError(error)),
-      take(1)
-    )
-    .subscribe({
-      next: (backups: Backup[]) => {
-        this.$backups.next(backups);
-        this._toast.show('success', `Бэкап файла ${current.file.name} успешно удален`, current.file.path, faClipboardCheck);
-        this.closeView();
-      },
-      error: (err) => {
-        console.error(err);
-        this._toast.show('danger', `Бэкап файла ${this.$currentSubject.value.file.name} не был удален по причине:`, err.message, faClipboard);
-      }
-    });
-  }
+	ngAfterViewInit(): void {
+		this.$backups.subscribe((backups: Backup[]) => {
+			if (this._graph) this._graph.redraw();
+			if (!backups) return;
+			
+			const size = backups.reduce((acc, curr) => curr.file.bytes ? acc + curr.file.bytes: acc, 0);
+			this.$backupsSize.next(size);
+		});
+	}
 
-  public restoreBackup(): void {
-    const current: Backup = this.$currentSubject.value;
-    
-    const dialogOpts: Electron.MessageBoxOptions = {
-      type: 'warning',
-      buttons: ['Да, установить', 'Отмена'],
-      title: `Подтверждение установки бэкапа`,
-      message: `Вы точно хотите установиить файл бэкапа ${current.file.name}? После подтверждения файл бэкапа ЗАМЕНИТ собой текущий файл ${this.$currentSubject.value.file.path}.`
-    };
-    
-    from(this._electron.ipcRenderer.invoke('message-box', dialogOpts))
-      .pipe(
-        switchMap((val) => iif(() => val.response == 0, this._api.restoreBackup(current.hash), throwError(() => new Error('Операция отменена пользователем')))),
-        catchError(error => handleError(error)),
-        take(1)
-      )
-      .subscribe({
-        next: () => {
-          this._toast.show('success', `Бэкап файла ${current.file.name} успешно установлен`, current.file.path, faClipboardCheck);
-        },
-        error: (err) => {
-          console.error(err);
-          this._toast.show('danger', `Бэкап файла ${current.file.name} не был установлен по причине:`, err.message, faClipboard);
-        }
-      });
-  }
-  
-  private _getBackupFile(hash: string): Observable<string> {
-    return this._api.getBackupFile(hash)
-                    .pipe(
-                      catchError(error => this.__handleError(error)),
-                      take(1)
-                    );
-  }
-
-  ngOnInit(): void {
-    of(window.localStorage.getItem('settings'))
-      .pipe(
-        filter((settings: string | null) => settings !== null),
-        map((settings: string) => JSON.parse(settings)),
-        take(1),
-      )
-      .subscribe(({ textEditorStyle }) => {
-        this.codeMirrorSettings.theme = textEditorStyle;
-      });
-      
-      this._api.getBackupsList()
-               .pipe(
-                  take(1),
-               )
-               .subscribe((backups: Backup[]) => {
-                this.$backups.next(backups);
-                if (backups.length) this.showBackupView(backups[0]);
-               });
-  }
-
-  ngAfterViewInit(): void {
-    this.$backups.subscribe((backups: Backup[]) => {
-      if (this._graph) this._graph.redraw();
-      if (!backups) return;
-      
-      const size = backups.reduce((acc, curr) => curr.file.bytes ? acc + curr.file.bytes: acc, 0);
-      this.$backupsSize.next(size);
-    });
-  }
-
-  ngOnDestroy() {
-    this.$backups.unsubscribe();
-    this.$backupsSize.unsubscribe();
-  }
+	ngOnDestroy() {
+		this.$backups.unsubscribe();
+		this.$backupsSize.unsubscribe();
+	}
 
 }
